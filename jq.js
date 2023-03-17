a0711655850ed53fbb5c332bc12c100622ffa607
Horizon719 <milichovszky.sandor@gmail.com>
2023-03-17 10:54:20 +0100

    index.html és lista jqueryben

---
+++ jq.js
@@ -0,0 +1,26 @@
+let lista = [
+    {
+        nev: "",
+        tul1: "",
+        tul2: "",
+    },
+    {
+        nev: "",
+        tul1: "",
+        tul2: "",
+    },
+    {
+        nev: "",
+        tul1: "",
+        tul2: "",
+    },
+    {
+        nev: "",
+        tul1: "",
+        tul2: "",
+    },
+];
+
+$(function(){
+    
+})
